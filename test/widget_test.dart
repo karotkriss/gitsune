6f974f16157f0a549b8b0e2ae1b23d51acdaee18
Christopher McKay <101884182+karotkriss@users.noreply.github.com>
2026-08-04 04:50:43 -0400

    no-mistakes: apply CI fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gitsune/core/lock/app_lock.dart';
 import 'package:gitsune/main.dart';
+
+import 'support/fake_biometric_authenticator.dart';
+import 'support/memory_secure_storage.dart';
 
 void main() {
   testWidgets('boots to a dark-themed shell with four tabs', (tester) async {
-    await tester.pumpWidget(const GitsuneApp());
+    final appLock = AppLockController(
+      authenticator: FakeBiometricAuthenticator(),
+      storage: MemorySecureStorage(),
+    );
+    addTearDown(appLock.dispose);
+    await appLock.load();
+
+    await tester.pumpWidget(GitsuneApp(appLockController: appLock));
     await tester.pumpAndSettle();
 
     final materialApp = tester.widget<MaterialApp>(find.byType(MaterialApp));
